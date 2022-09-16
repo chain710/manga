f81accb2c106b6f19482dd1aa728c088c570cce6
chain710 <chain710@gmail.com>
2022-09-16 23:42:26 +0800

    add fav icons

--- view/vue.config.js
+++ view/vue.config.js
@@ -28,4 +28,35 @@ module.exports = defineConfig({
       title: "MangaDepot",
     },
   },
+
+  pwa: {
+    name: "MangaDepot",
+    assetsVersion: "1.1",
+    themeColor: "#ffffff",
+    msTileColor: "#da532c",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    manifestOptions: {
+      icons: [
+        {
+          src: "android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+    iconPaths: {
+      faviconSVG: "favicon.svg",
+      favicon32: "favicon-32x32.png",
+      favicon16: "favicon-16x16.png",
+      appleTouchIcon: "apple-touch-icon.png",
+      maskIcon: "safari-pinned-tab.svg",
+      msTileImage: null,
+    },
+  },
 });
